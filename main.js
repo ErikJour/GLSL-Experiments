3dee802b079ccc8234ee470287329cc21bdf99ed
Erik Jourgensen <erikjourgensen@gmail.com>
2026-04-16 11:45:44 -0700

    Ready to go

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 import * as THREE from 'three'
-
+import GUI from 'lil-gui'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+//===============================
+//Debug
+//===============================
+const gui = new GUI();
+//===============================
+//Canvas
+//===============================
 const canvas = document.querySelector('canvas.webgl');
-
+//===============================
+//Scene
+//===============================
 const scene = new THREE.Scene();
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-
+//===============================
+//Test Geo
+//===============================
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+//===============================
+//Materials
+//===============================
 const material = new THREE.MeshBasicMaterial();
 material.color = new THREE.Color(0xff0000);
 const mesh = new THREE.Mesh(geometry, material);
@@ -16,14 +30,35 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+//===============================
+//Camera
+//===============================
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(0.25, -0.25, 1)
 scene.add(camera);
-
+//===============================
+//Controls
+//===============================
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+//===============================
+//Renderer
+//===============================
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-renderer.render(scene, camera);
+//===============================
+//Animate
+//===============================
+const clock = new THREE.Clock();
+const animate = () => {
+    const elapsedTime = clock.getElapsedTime();
+    controls.update();
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(animate);
+}
+
+animate();
