7f9d74a9c9eeb91927ba42908213ed15a36e9477
Erik Jourgensen <erikjourgensen@gmail.com>
2026-04-21 13:31:29 -0700

    Tuesday shaders

--- Shaders/fragment.glsl
+++ Shaders/fragment.glsl
@@ -1,8 +1,26 @@
-precision mediump float;
-
-uniform vec3 uColor;
+varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor = vec4(uColor, 1.0);
+    //Pattern 3
+    float strength = vUv.x;
+    //Pattern 4
+    float weight = vUv.y;
+    //Pattern 5
+    float upsideDownWeight = 1.0 - weight;
+    //Pattern 6
+    float fan = mod(weight * 10.0, 1.5);
+    //Pattern 7
+    float fanTwo = mod(weight * 10.0, 1.0);
+    fanTwo = step(0.9, fanTwo); //Very intersting for modulations
+
+    //Pattern 10
+    float fanThree = mod(strength * 10.0, 1.0);
+    fanThree = step(0.9, fanThree);
+
+    //Pattern 11 - Grid
+    float fanFour = step(0.8, mod(weight * 10.0, 1.0));
+    fanFour -= step(0.6, mod(strength * 10.0, 1.0));
+
+    gl_FragColor = vec4(fanFour, fanFour, fanFour, 1.0);
 }
--- main.js
+++ main.js
@@ -23,21 +23,12 @@ const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 //===============================
 //Materials
 //===============================
-const material = new THREE.RawShaderMaterial({
+const material = new THREE.ShaderMaterial({
     vertexShader: vertexShader,
     fragmentShader: fragmentShader,
-    uniforms:
-        {
-            uFrequency: { value: new THREE.Vector2(10, 5) },
-            uTime: { value: 0 },
-            uColor: { value: new THREE.Color('orange') }
-        }
+    side: THREE.DoubleSide
 });
 
-gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX');
-gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY');
-
-material.color = new THREE.Color(0xff0000);
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
@@ -74,8 +65,6 @@ const animate = () => {
     clock.update();
     const elapsedTime = clock.getElapsed();
    //Update Material
-    material.uniforms.uTime.value = elapsedTime;
-    material.uniforms.uColor.value = new THREE.Vector3(Math.sin(elapsedTime), Math.sin(elapsedTime) * 0.3, 1);
 
     controls.update();
     renderer.render(scene, camera);
