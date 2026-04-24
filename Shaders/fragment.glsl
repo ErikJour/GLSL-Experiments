d0098ce80b4c31faf11130a6a0627b1b357c695a
Erik Jourgensen <erikjourgensen@gmail.com>
2026-04-24 12:47:40 -0700

    More shader patterns

--- Shaders/fragment.glsl
+++ Shaders/fragment.glsl
@@ -1,5 +1,10 @@
 varying vec2 vUv;
 
+float random(vec2 st)
+{
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.543123);
+}
+
 void main()
 {
     //Pattern 3
@@ -22,5 +27,55 @@ void main()
     float fanFour = step(0.8, mod(weight * 10.0, 1.0));
     fanFour -= step(0.6, mod(strength * 10.0, 1.0));
 
-    gl_FragColor = vec4(fanFour, fanFour, fanFour, 1.0);
+    //Pattern 14
+    float barX = step(0.4, mod(strength * 10.0, 1.0));
+    barX *= step(0.8, mod(weight * 10.0, 1.0));
+    float barY = step(0.4, mod(weight * 10.0, 1.0));
+    barY *= step(0.8, mod(strength * 10.0, 1.0));
+
+    float newStrength = barX + barY;
+
+    //Pattern 15
+    float barFifteenX = step(0.4, mod(strength * 10.0 - 0.2, 1.0));
+    barFifteenX *= step(0.8, mod(weight * 10.0, 1.0));
+
+    float barFifteenY = step(0.4, mod(weight * 10.0 - 0.2, 1.0));
+    barFifteenY *= step(0.8, mod(strength * 10.0, 1.0));
+
+    float patternFifteen = barFifteenX + barFifteenY;
+
+    //Pattern 16
+    float patternSixteen = abs(vUv.x - 0.5);
+
+    //Pattern 17
+    float patternSeventeenX = abs(vUv.x - 0.5);
+    float patternSeventeenY = abs(vUv.y - 0.5);
+    float patternSeventeen = min(patternSeventeenX, patternSeventeenY);
+
+    //Pattern 18
+    float patternEighteenX = abs(vUv.x - 0.5);
+    float patternEighteenY = abs(vUv.y - 0.5);
+    float patternEighteen = max(patternEighteenX, patternEighteenY);
+
+    //Pattern 19
+    float patternNineteenX = abs(vUv.x - 0.5);
+    float patternNineteenY = abs(vUv.y - 0.5);
+    float patternNineteen = step(0.2, max(patternEighteenX, patternEighteenY));
+
+    //Pattern 20
+    float patternTwentyX = abs(vUv.x - 0.5);
+    float patternTwentyY = abs(vUv.y - 0.5);
+    float patternTwenty = step(0.4, max(patternEighteenX, patternEighteenY));
+
+    //Pattern 21
+    float round = floor(vUv.x * 10.0) / 10.0;
+
+    //Pattern 22
+    float roundSquare = floor(vUv.x * 10.0) / 10.0;
+    roundSquare *= floor(vUv.y * 10.0) / 10.0;
+
+    //Pattern 23
+    float randomNoise = random(vUv);
+
+    gl_FragColor = vec4(randomNoise, randomNoise, randomNoise, 1.0);
 }
